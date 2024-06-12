4a30b2c7e078baa18838ab0467540e82cf58e44a
KUmAR <94027452+kumar238@users.noreply.github.com>
2024-06-12 16:29:22 +0530

    Update automation2.sh

--- automation2.sh
+++ automation2.sh
@@ -2,5 +2,5 @@
 version="1.2.3.4"
 for line in $(cat jiraid.txt)
 do
-curl -X PUT -u "your_atlassian_jira_username:your_atlassian_jira_token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/$line
+curl -X PUT -u "kumar0811sg@gmail.com:ATATT3xFfGF05lMI0Zz1iuRsaQwpCziWUkWf3QIYUt5Om3jblUz7QAOMhV1vx-tn9a_pMvuo5BbV8x8jIZFqjudG8czgfh-DpylBoNtsg3S93IK61jgWTNenOTV_RtMqPvhfXUFA147pNCzuEsa-gcvfIKGj2xPUJADTiUURucYRXyYI-6iKfWM=65DEA894" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://kumar0811sh.atlassian.net/rest/api/3/issue/KA-11
 done 
